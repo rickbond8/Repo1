db04fdfdad143c5f086d1b0bfb690846a76cc776
Rick <Rick@192.168.0.17>
2018-04-06 14:43:39 -0400

    1111

---
+++ GradleTest/src/main/java/com/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		int i = 1;
+		System.out.println(i + 1.5*3.0);
+	}
+
+}
